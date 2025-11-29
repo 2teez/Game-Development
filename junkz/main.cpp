5fabaf6ce40c220e32eb0b6148a5fe834179855f
timothy adigun <adigun.timothy@gmail.com>
2025-11-29 14:24:59 +0000

    basic of sfml c++ programming

---
+++ junkz/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char* argv[])
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "First Window!");
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    window.clear(sf::Color::Black);
+    window.display();
+
+    return 0;
+}
